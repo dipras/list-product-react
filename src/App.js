ab9f994acf8c41413e10df2588e38686b924762b
Dipras <dipras75@gmail.com>
2020-10-13 20:11:47 +0700

    delete router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Products from './components/Products';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => (<Products />)} />
-      </Switch>
-    </Router>
+    <Products />
   );
 }
 

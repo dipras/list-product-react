949ec1a80cc947d66a936e3c8e4cd6771a865af7
Dipras <dipras75@gmail.com>
2020-10-13 19:16:26 +0700

    Updates

---
+++ precache-manifest.61086f891f8c21197bfaf1dca8d75040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c090e833188ab0ea5c59ba53cb3976df",
+    "url": "/list-product-react/index.html"
+  },
+  {
+    "revision": "d9016a819bff70306161",
+    "url": "/list-product-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2a33a7b6eeee17710cf3",
+    "url": "/list-product-react/static/js/2.098451f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/list-product-react/static/js/2.098451f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9016a819bff70306161",
+    "url": "/list-product-react/static/js/main.3ae0002e.chunk.js"
+  },
+  {
+    "revision": "e3752cff394f7ddf2394",
+    "url": "/list-product-react/static/js/runtime-main.fe1a6034.js"
+  }
+]);
